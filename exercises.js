7773086ae40e1634332e708ed9115578a259ea66
TugceBaydar <31819490+TugceBaydar@users.noreply.github.com>
2017-09-24 08:25:50 -0700

    Update exercises.js

--- exercises.js
+++ exercises.js
@@ -3,10 +3,13 @@
 function multiplyByTen(num) {
   // return num after multiplying it by ten
   // code here
+
+return num*10;
 }
 
 function subtractFive(num) {
   // return num after subtracting five
+  return num-5;
   // code here
 }
 
@@ -14,126 +17,176 @@ function areSameLength(str1, str2) {
   // return true if the two strings have the same length
   // otherwise return false
   // code here
+  //
+
+  return str1.length===str2.length;
 }
 
 function areEqual(x, y) {
   // return true if x and y are the same
   // otherwise return false
   // code here
+  return x === y;
 }
 
 function lessThanNinety(num) {
   // return true if num is less than ninety
   // otherwise return false
   // code here
+  return num < 90;
 }
 
 function greaterThanFifty(num) {
   // return true if num is greater than fifty
   // otherwise return false
   // code here
+  return num > 50;
 }
 
 function add(x, y) {
   // add x and y together and return the value
   // code here
+ return x+y;
 }
 
 function subtract(x, y) {
   // subtract y from x and return the value
   // code here
+  return x-y;
 }
 
 function divide(x, y) {
   // divide x by y and return the value
+return x/y;
   // code here
 }
 
 function multiply(x, y) {
   // multiply x by y and return the value
   // code here
+
+  return x*y;
+
 }
 
 function getRemainder(x, y) {
   // return the remainder from dividing x by y
   // code here
+  return x%y;
 }
 
 function isEven(num) {
   // return true if num is even
   // otherwise return false
   // code here
+  return num%2 === 0;
 }
 
 function isOdd(num) {
   // return true if num is false
   // otherwise return false
   // code here
+
+  return !isEven(num);
 }
 
 function square(num) {
   // square num and return the new value
   // code here
+  return num*num;
 }
 
 function cube(num) {
   // cube num and return the new value
   // code here
+  return square(num)*num;
 }
 
 function raiseToPower(num, exponent) {
   // raise num to whatever power is passed in as exponent
   // code here
+var total = 1;
+
+  for (var i=0;i<exponent;i++) {
+    total=num*total;
+  }
+ return total;
+
 }
 
 function roundNumber(num) {
   // round num and return it
   // code here
+
+  var remainder = num%1;
+  if (remainder>=0.5) {
+    return 1-remainder+num;
+  }
+  else {
+    return num-remainder;
+  }
 }
 
 function roundUp(num) {
   // round num up and return it
   // code here
-}
+  var remainder = num%1;
 
+  if (remainder>0)
+  {
+    return (num+1-remainder);
+  }
+  else {
+    return num;
+  }
+}
 function addExclamationPoint(str) {
   // add an exclamation point to the end of str and return the new string
   // 'hello world' -> 'hello world!'
   // code here
+return str + '!';
 }
 
 function combineNames(firstName, lastName) {
   // return firstName and lastName combined as one string and separated by a space.
   // 'Lambda', 'School' -> 'Lambda School'
   // code here
+  return firstName + ' '  + lastName;
 }
 
 function getGreeting(name) {
   // Take the name string and concatenate other strings onto it so it takes the following form:
   // 'Sam' -> 'Hello Sam!'
   // code here
+  return 'Hello ' + name + '!';
 }
 
 // If you can't remember these area formulas then head over to Google or look at the test code.
- 
+
 function getRectangleArea(length, width) {
   // return the area of the rectangle by using length and width
   // code here
+return length*width;
+
 }
 
 function getTriangleArea(base, height) {
   // return the area of the triangle by using base and height
   // code here
+
+  return (base*height)/2;
 }
 
 function getCircleArea(radius) {
   // return the rounded area of the circle given the radius
   // code here
+  return  roundNumber(Math.PI*radius*radius);
 }
 
 function getRectangularPrismVolume(length, width, height) {
   // return the volume of the 3D rectangular prism given the length, width, and height
   // code here
+  return length*width*height;
 }
 
 // Do not modify code below this line.
@@ -166,3 +219,6 @@ module.exports = {
   getCircleArea: getCircleArea,
   getRectangularPrismVolume: getRectangularPrismVolume
 };
+
+
+
